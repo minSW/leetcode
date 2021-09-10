091428c0b5a9f55d2fbe15cda47edb0de1be86ce
minSW <minsw08@gmail.com>
2021-09-11 00:07:26 +0900

    [210910] EASY 1854 Solved

--- solution/1854_Maximum_Population_Year.py
+++ solution/1854_Maximum_Population_Year.py
@@ -1,7 +1,18 @@
 class Solution:
+    # O(n)
     def maximumPopulation(self, logs: List[List[int]]) -> int:
         population = [0] * 101
         for start, end in logs :
-            for i in range(start - 1950, end - 1950) :
-                population[i] += 1
+            population[start-1950] += 1
+            population[end-1950] -= 1
+        for i in range(1, len(population)) :
+            population[i] += population[i-1]
         return population.index(max(population)) + 1950
+        
+    # O(n^2)
+    # def maximumPopulation(self, logs: List[List[int]]) -> int:
+    #     population = [0] * 101
+    #     for start, end in logs :
+    #         for i in range(start - 1950, end - 1950) :
+    #             population[i] += 1
+    #     return population.index(max(population)) + 1950
