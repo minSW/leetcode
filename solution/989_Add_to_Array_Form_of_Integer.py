2aceb7baf9e908a137c72395bb7c2f8037b6dfde
minSW <minsw08@gmail.com>
2021-09-23 00:04:01 +0900

    [210922] EASY 989 Solved

--- solution/989_Add_to_Array_Form_of_Integer.py
+++ solution/989_Add_to_Array_Form_of_Integer.py
@@ -1,17 +1,15 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        carry = 0
-        i = len(num)-1
+        carry, i = 0, len(num) - 1
+        
         while k or carry :
             if i < 0 :
-                num.insert(0, k % 10 + carry)
+                num.insert(0, 0)
                 i = 0
-            else :
-                num[i] += k % 10 + carry
-            carry = 0
+            num[i] += k % 10 + carry
+            carry, k = 0, k // 10
             if num[i] >= 10 :
-                carry = 1
-                num[i] -= 10
-            k //= 10
+                carry, num[i] = 1, num[i] % 10
             i -= 1
+        
         return num
